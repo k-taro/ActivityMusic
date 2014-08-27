d9ed512ce49164a7617ffb14da5779d98df0d897
k-taro <can8anyone4hear8me1@yahoo.co.jp>
2014-08-27 10:37:03 +0900

    インポートの最適化

--- app/src/main/java/com/keitaro/activitymusic/activity/MyActivity.java
+++ app/src/main/java/com/keitaro/activitymusic/activity/MyActivity.java
@@ -1,13 +1,13 @@
 package com.keitaro.activitymusic.activity;
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
